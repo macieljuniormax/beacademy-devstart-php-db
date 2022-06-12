4036238f68353284059956a77836ae3309df4de8
Maciel Júnior <macielfcjunior@icloud.com>
2022-06-12 13:26:21 -0300

    feat: Inserindo dados na tabela

--- Aula 01/db_escola.sql
+++ Aula 01/db_escola.sql
@@ -7,6 +7,53 @@ USE db_escola;
 -- Criar uma tabela --
 CREATE TABLE tb_professor(
   nome VARCHAR(128) NOT NULL,
-  cpf CHAR(11) NOT NULL,
-  email VARCHAR(128) NOT NULL
+  cpf CHAR(11) UNIQUE NOT NULL,
+  email VARCHAR(128) UNIQUE NOT NULL
 );
+
+CREATE TABLE tb_aluno(
+  nome VARCHAR(128) NOT NULL,
+  cpf CHAR(11) NOT NULL,
+  email VARCHAR(128) NOT NULL,
+  matricula VARCHAR(10) NOT NULL
+);
+
+-- Inserir Dados --
+INSERT INTO
+  tb_professor(nome, email, cpf)
+VALUES
+  ('Maciel', 'maciel@email.com', '12345678912');
+
+INSERT INTO
+  tb_professor(nome, email, cpf)
+VALUES
+  ('Bruno', 'bruno@email.com', '12345678913');
+
+INSERT INTO
+  tb_aluno(nome, email, cpf, matricula)
+VALUES
+  (
+    'Rodrigo',
+    'rodrigo@email.com',
+    '12345678913',
+    '1234567891'
+  );
+
+INSERT INTO
+  tb_aluno(nome, email, cpf, matricula)
+VALUES
+  (
+    'Alessandro',
+    'alessandro@email.com',
+    '12345678913',
+    '1234567891'
+  );
+
+-- Excluir Tabela --
+DROP TABLE IF EXISTS `tb_professor`;
+
+-- Buscar Dados --
+SELECT
+  *
+FROM
+  tb_professor;
